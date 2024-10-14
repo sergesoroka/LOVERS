93f25d3557f2d22c092e9c8597a07921e092d781
sergesoroka <sergesoroka@gmail.com>
2024-10-14 18:15:10 +0300

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "#1400ff",
         foreground: "var(--foreground)",
       },
     },
