3a5a12cdff0ff6b35f7c3ec7dca712cd4b7160c6
sergesoroka <sergesoroka@gmail.com>
2024-11-02 12:34:59 +0200

    About Page, removed unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Link from "next/link";
 import MainLogo from "@/components/Logo/MainLogo";
 import PlayIcon from "@/components/Icons/PlayIcon";
 import Header from "@/components/Header";
 import MediaLinks from "@/components/MediaLinks";
 
 export default async function Home() {
-  // const data = await fetch("http://lovers.company/api/wp-json/wp/v2/videos");
-  // const videos = await data.json();
-
   return (
     <div className="home-page">
       <div className="absolute top-[22px] left-[24px] right-[24px]">
@@ -22,35 +18,6 @@ export default async function Home() {
       </div>
       <div className="absolute bottom-[22px] right-[24px] text-[#1400ff] text-right">
         <MediaLinks />
-        {/* <ul className="text-right uppercase text-[#1400ff]">
-          <li>
-            <Link
-              href="https://vimeo.com/loverscompany"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              vimeo
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://www.instagram.com/production.lovers.company/?igsh=MWxmbjFuNWttdm1vZw%3D%3D"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              instagram
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://www.linkedin.com/company/loverscompany"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              linked in
-            </Link>
-          </li>
-        </ul> */}
       </div>
     </div>
   );
