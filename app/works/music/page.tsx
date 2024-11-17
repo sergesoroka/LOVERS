1288c0bf2bee3d973b2dbecb664b88ad9dca682b
sergesoroka <sergesoroka@gmail.com>
2024-11-17 17:20:30 +0200

    Added Category Pages, removed idx, music page

--- app/works/music/page.tsx
+++ app/works/music/page.tsx
@@ -9,8 +9,6 @@ export default async function WorksMusic() {
   });
   const videos = await data.json();
 
-  let idx = 0;
-
   return (
     <div className="pt-[22px] px-[24px] bg-black relative">
       <Header />
@@ -20,7 +18,6 @@ export default async function WorksMusic() {
       <div className="md:grid grid-cols-2 gap-[75px] mt-28 mb-[164px] md:px-[56px]">
         {videos.map((video) => {
           if (video.type_of_work.rendered == "music") {
-            idx++;
             return (
               <div key={video.id}>
                 <VimeoPlayer
