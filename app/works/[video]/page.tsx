d8ab1a9899e8095373694e3d78cc3198177fa885
sergesoroka <sergesoroka@gmail.com>
2024-11-17 21:31:04 +0200

    Slider with slugs path, cleared

--- app/works/[video]/page.tsx
+++ app/works/[video]/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "@/components/Header";
-import VideoSlider from "@/components/VideoSlider/VideoSlider";
 import VideoSliderCategory from "@/components/VideoSlider/VideoSliderCategory";
 
 export default async function Video() {
@@ -14,7 +13,6 @@ export default async function Video() {
   return (
     <div className="pt-[22px] px-[24px] bg-black h-full">
       <Header />
-      {/* <VideoSlider videos={videos} /> */}
       <VideoSliderCategory videos={videos} slugs={slugsArray} category="" />
     </div>
   );
