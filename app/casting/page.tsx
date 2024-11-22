591dc4c7d3aec9ca1c58eda3819c6853e6962f60
sergesoroka <sergesoroka@gmail.com>
2024-11-22 20:20:29 +0200

    Pl data1

--- app/casting/page.tsx
+++ app/casting/page.tsx
@@ -1,42 +1,42 @@
-// import Header from "@/components/Header";
-// import ContactComp from "@/components/ContactComp/ContactComp";
+import Header from "@/components/Header";
+import ContactComp from "@/components/ContactComp/ContactComp";
 
-// export default async function Casting() {
-//   const data = await fetch("http://lovers.company/api/wp-json/wp/v2/pages");
-//   const pages = await data.json();
+export default async function Casting() {
+  //   const data = await fetch("http://lovers.company/api/wp-json/wp/v2/pages");
+  //   const pages = await data.json();
 
-//   return (
-//     <div className="casting-bg">
-//       <div className="casting-bg-img h-[853px] pt-[22px] px-[24px] ">
-//         <Header />
-//         {pages.map((page) => {
-//           if (page.id === 57) {
-//             return (
-//               <div key={page.id} className="px-[69px]">
-//                 <h1 className="contactsTitle text-[#1400ff] uppercase mt-[60px]  text-[160px] leading-[80px]">
-//                   {page.title.rendered}
-//                 </h1>
-//                 <h1 className="contactsTitle text-[#1400ff] uppercase mt-[60px]  text-[160px] leading-[80px]">
-//                   {page.title.rendered}
-//                 </h1>
-//                 <h1 className="contactsTitle text-[#1400ff] uppercase mt-[60px]  text-[160px] leading-[80px]">
-//                   {page.title.rendered}
-//                 </h1>
+  return (
+    <div className="casting-bg">
+      <div className="casting-bg-img h-[853px] pt-[22px] px-[24px] ">
+        <Header />
+        {/* {pages.map((page) => {
+          if (page.id === 57) {
+            return (
+              <div key={page.id} className="px-[69px]">
+                <h1 className="contactsTitle text-[#1400ff] uppercase mt-[60px]  text-[160px] leading-[80px]">
+                  {page.title.rendered}
+                </h1>
+                <h1 className="contactsTitle text-[#1400ff] uppercase mt-[60px]  text-[160px] leading-[80px]">
+                  {page.title.rendered}
+                </h1>
+                <h1 className="contactsTitle text-[#1400ff] uppercase mt-[60px]  text-[160px] leading-[80px]">
+                  {page.title.rendered}
+                </h1>
 
-//                 <div
-//                   className="z-10 uppercase text-[40px] leading-[44px] mt-[-60px] pl-[125px] pr-[290px] pt-14"
-//                   dangerouslySetInnerHTML={{ __html: page.content.rendered }}
-//                 />
-//                 <h1 className="contactsTitle z-0 text-[#1400ff] uppercase mt-[-10px]  text-[160px] leading-[80px]">
-//                   {page.title.rendered}
-//                 </h1>
-//               </div>
-//             );
-//           }
-//         })}
+                <div
+                  className="z-10 uppercase text-[40px] leading-[44px] mt-[-60px] pl-[125px] pr-[290px] pt-14"
+                  dangerouslySetInnerHTML={{ __html: page.content.rendered }}
+                />
+                <h1 className="contactsTitle z-0 text-[#1400ff] uppercase mt-[-10px]  text-[160px] leading-[80px]">
+                  {page.title.rendered}
+                </h1>
+              </div>
+            );
+          }
+        })} */}
 
-//         <ContactComp mode="light" />
-//       </div>
-//     </div>
-//   );
-// }
+        <ContactComp mode="light" />
+      </div>
+    </div>
+  );
+}
--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -1,28 +1,28 @@
-// import Header from "@/components/Header";
-// import ContactComp from "@/components/ContactComp/ContactComp";
+import Header from "@/components/Header";
+import ContactComp from "@/components/ContactComp/ContactComp";
 
-// export default async function Clients() {
-//   // const data = await fetch("http://lovers.company/api/wp-json/wp/v2/pages");
-//   // const pages = await data.json();
+export default async function Clients() {
+  // const data = await fetch("http://lovers.company/api/wp-json/wp/v2/pages");
+  // const pages = await data.json();
 
-//   return (
-//     <div className="pt-[22px] px-[24px] bg-[#151515]">
-//       <Header />
-//       CLIENTS
-//       {/* <div>
-//         {pages.map((page) => {
-//           if (page.id === 2) {
-//             return (
-//               <div
-//                 key={page.id}
-//                 className="uppercase text-[40px] leading-[44px] px-[195px] pt-14"
-//                 dangerouslySetInnerHTML={{ __html: page.content.rendered }}
-//               />
-//             );
-//           }
-//         })}
-//       </div> */}
-//       <ContactComp mode="light" />
-//     </div>
-//   );
-// }
+  return (
+    <div className="pt-[22px] px-[24px] bg-[#151515]">
+      <Header />
+      CLIENTS
+      {/* <div>
+        {pages.map((page) => {
+          if (page.id === 2) {
+            return (
+              <div
+                key={page.id}
+                className="uppercase text-[40px] leading-[44px] px-[195px] pt-14"
+                dangerouslySetInnerHTML={{ __html: page.content.rendered }}
+              />
+            );
+          }
+        })}
+      </div> */}
+      <ContactComp mode="light" />
+    </div>
+  );
+}
