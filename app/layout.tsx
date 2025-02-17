106b81f6978c747bcef2e6a95d7086ef33ecb602
sergesoroka <sergesoroka@gmail.com>
2025-02-17 15:05:37 +0200

    Added favicon for Safari

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" /> */}
+        <link rel="icon" href="favicon.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
