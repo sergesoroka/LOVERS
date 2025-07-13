c6a60d1dce28f4c4078b20a1aa08ded71a06010e
sergesoroka <sergesoroka@gmail.com>
2025-07-13 16:28:57 +0300

    mobile menu styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
   return (
     <>
       {open ? (
-        <div className="fixed  top-0 bottom-0 right-0 left-0 bg-[#1400ff] z-[999]">
+        <div className="fixed overflow-scroll top-0 bottom-0 right-0 left-0 bg-[#1400ff] z-[999]">
           <div className="md:flex justify-between pt-[22px] px-[24px] items-start">
             <div className="hidden md:block">
               <LogoMenu />
@@ -45,7 +45,7 @@ export default function Header() {
               </li>
             ))}
           </ul>
-          <div className="absolute bottom-[22px] right-[24px] text-black text-right text-[28px]">
+          <div className="md:absolute ml-16 mb-8 bottom-[22px] right-[24px] text-black md:text-right text-[28px]">
             <ul>
               <Link href="https://vimeo.com/loverscompany" target="_blank">
                 <li className="uppercase hover:line-through cursor-pointer hover:text-white z-50">
