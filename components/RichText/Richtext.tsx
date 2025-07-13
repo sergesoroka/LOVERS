b57f4213ad036907321b582e53fab958e59a96ad
sergesoroka <sergesoroka@gmail.com>
2025-07-13 16:47:59 +0300

    Removed category pages

--- components/RichText/Richtext.tsx
+++
@@ -1,6 +0,0 @@
-// "use client";
-// import { RichText } from "@payloadcms/richtext-lexical/react";
-
-// export default function RichTextComp({ data }) {
-//   return <RichText data={data} />;
-// }
