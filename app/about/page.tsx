40ab8b3ea69aaf2c21b179ab443d180580ff118f
sergesoroka <sergesoroka@gmail.com>
2025-09-25 08:24:04 +0300

    Adjusted menu

---
+++ app/about/page.tsx
@@ -0,0 +1,35 @@
+import Header from "@/components/Header";
+import ContactComp from "@/components/ContactComp/ContactComp";
+import ClientsComp from "@/components/ClientsComp/ClientsComp";
+
+export default async function About() {
+  const data = await fetch(
+    "https://lovers-payload-cms.vercel.app/api/contacts"
+  );
+  const lovers = await data.json();
+
+  return (
+    <div className="relative">
+      <div className="pt-[22px] bg-[#1400ff]">
+        <div className="px-[24px]">
+          <Header />
+        </div>
+
+        <div className="my-20  px-[24px]">
+          <h2 className="pagesTitle text-[#151515] ml-11 contactsTitle uppercase text-[98px] leading-[84px] md:mt-[60px]  md:text-[160px] md:leading-[140px]">
+            About
+          </h2>
+          <div className="uppercase text-[32px] leading-[34px] mt-[-48px] md:text-[40px] md:leading-[44px]">
+            <div className="uppercase text-[32px] leading-[34px] md:text-[40px] md:leading-[44px]  md:px-[170px]">
+              {lovers?.docs[0]?.contact}
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#151515] pt-20 pb-10 h-full w-full">
+          <ClientsComp />
+        </div>
+        <ContactComp mode="dark" />
+      </div>
+    </div>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -15,13 +15,12 @@ import { useGSAP } from "@gsap/react";
 // gsap.registerPlugin(ScrollTrigger);
 
 const menuItems = [
-  { id: "0", link: "lovers" },
-  { id: "1", link: "works" },
+  { id: "0", link: "works" },
+  { id: "1", link: "about" },
   { id: "2", link: "clients" },
   { id: "3", link: "locations" },
   { id: "4", link: "casting" },
-  { id: "5", link: "partners" },
-  { id: "6", link: "contacts" },
+  { id: "5", link: "contacts" },
 ];
 export default function Header() {
   const [open, setOpen] = useState(false);
@@ -34,8 +33,8 @@ export default function Header() {
   return (
     <>
       {open ? (
-        <div className="fixed overflow-scroll md:overflow-hidden top-0 bottom-0 right-0 left-0 bg-[#1400ff] z-[999]">
-          <div className="md:flex justify-between pt-[22px] px-[24px] items-start">
+        <div className="fixed pt-[42px] px-[38px] overflow-scroll md:overflow-hidden top-0 bottom-0 right-0 left-0 bg-[#1400ff] z-[999]">
+          <div className="md:flex justify-between items-start">
             <div className="logo hidden md:block">
               <Link href={"/"}>
                 <LogoMenu />
@@ -43,45 +42,25 @@ export default function Header() {
             </div>
             <button
               onClick={() => setOpen(false)}
-              className="absolute top-[28px] right-[38px] uppercase text-white cursor-pointer select-none"
+              className="uppercase text-white cursor-pointer select-none"
             >
               close
             </button>
           </div>
-          <ul className="md:flex flex-wrap gap-4 mobMenu uppercase mb-8 px-[18px] mt-[60px] ml-[43px]">
-            {menuItems.map((item) => (
-              <li
-                key={item.id}
-                className="menuItems text-white text-[90px] md:text-[180px] md:mb-[60px]"
-              >
-                <Link href={`/${item.link}`}>{item.link}</Link>
-              </li>
-            ))}
-          </ul>
-          <div className="md:absolute ml-16 mb-8 bottom-0 right-[24px] text-black md:text-right text-[28px]">
-            <ul>
-              <Link href="https://vimeo.com/loverscompany" target="_blank">
-                <li className="uppercase hover:line-through cursor-pointer hover:text-white z-50">
-                  vimeo
+          <div className="md:flex b-8 justify-between items-end mt-[60px] ml-[43px]">
+            <ul className="md:flex flex-wrap gap-12 mobMenu uppercase">
+              {menuItems.map((item) => (
+                <li
+                  key={item.id}
+                  className="menuItems text-white text-[90px] md:text-[180px] md:mb-[30px]"
+                >
+                  <Link href={`/${item.link}`}>{item.link}</Link>
                 </li>
-              </Link>
-              <Link
-                href="https://www.instagram.com/production.lovers.company/?igsh=MWxmbjFuNWttdm1vZw%3D%3D#"
-                target="_blank"
-              >
-                <li className="uppercase hover:line-through cursor-pointer hover:text-white z-50">
-                  instagram
-                </li>
-              </Link>
-              <Link
-                href="https://www.linkedin.com/company/loverscompany"
-                target="_blank"
-              >
-                <li className="uppercase hover:line-through cursor-pointer hover:text-white z-50">
-                  linkedin
-                </li>
-              </Link>
+              ))}
             </ul>
+            <div className=" text-black md:text-right text-[28px]">
+              <SocialMedia />
+            </div>
           </div>
         </div>
       ) : (
@@ -112,48 +91,37 @@ export default function Header() {
       )}
       <div className="fixed right-[24px] left-[24px] z-50 flex justify-between items-center">
         <Link href="/">
-          <div className="flex items-center justify-between">
-            {/* <div className="hidden md:flex items-center justify-between">
-              <Logo />
-            </div>
-
-            <div
-              className={`hidden md:block uppercase ml-[11px] text-[18px] ${
-                pathname == "/" ? "text-[#1400ff]" : "text-white"
-              } `}
-            >
-              From passion to action
-            </div> */}
-          </div>
+          <div className="flex items-center justify-between"></div>
         </Link>
-
-        {/* <ul className="hidden md:flex justify-end items-center gap-7 uppercase">
-          {menuItems.map((item) => (
-            <li
-              key={item.id}
-              className={`cursor-pointer hover:line-through ${
-                pathname == "/" + item.link &&
-                pathname !== "/works" &&
-                pathname !== "/locations" &&
-                pathname !== "/casting" &&
-                pathname !== "/clients"
-                  ? "text-black line-through"
-                  : pathname == "/works" && item.link == "works"
-                  ? "text-[#1400ff]  line-through"
-                  : pathname == "/locations" && item.link == "locations"
-                  ? "text-white  line-through"
-                  : pathname == "/casting" && item.link == "casting"
-                  ? "text-white  line-through"
-                  : pathname == "/clients" && item.link == "clients"
-                  ? "text-[#1400ff]  line-through"
-                  : "text-white"
-              } `}
-            >
-              <Link href={`/${item.link}`}>{item.link}</Link>
-            </li>
-          ))}
-        </ul> */}
       </div>
     </>
   );
 }
+
+const SocialMedia = () => {
+  return (
+    <ul>
+      <li className="uppercase hover:line-through cursor-pointer hover:text-white z-50">
+        <Link href="https://vimeo.com/loverscompany" target="_blank">
+          vimeo
+        </Link>
+      </li>
+      <Link
+        href="https://www.instagram.com/production.lovers.company/?igsh=MWxmbjFuNWttdm1vZw%3D%3D#"
+        target="_blank"
+      >
+        <li className="uppercase hover:line-through cursor-pointer hover:text-white z-50">
+          instagram
+        </li>
+      </Link>
+      <Link
+        href="https://www.linkedin.com/company/loverscompany"
+        target="_blank"
+      >
+        <li className="uppercase hover:line-through cursor-pointer hover:text-white z-50">
+          linkedin
+        </li>
+      </Link>
+    </ul>
+  );
+};
