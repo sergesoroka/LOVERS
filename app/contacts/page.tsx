288c02937f7a1ce4806967e112d0017a74106bc6
sergesoroka <sergesoroka@gmail.com>
2025-09-25 17:57:19 +0300

    Contact, removed scroll

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -3,8 +3,9 @@ import ContactComp from "@/components/ContactComp/ContactComp";
 
 export default async function Home() {
   return (
-    <div className="pt-[22px] px-[24px] bg-[#1400ff]">
+    <div className="pt-[22px] px-[24px] bg-[#1400ff] overflow-hidden h-screen">
       <Header />
+
       <ContactComp mode="dark" />
     </div>
   );
